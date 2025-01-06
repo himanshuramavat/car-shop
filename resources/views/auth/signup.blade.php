434eea6daa501baeebb17fff4112eb9f275029de
Himanshu Ramavat <himanshuramavat07@gmail.com>
2025-01-06 21:45:20 +0530

    Add guest layout and social authentication buttons for login and signup pages

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,14 +1,6 @@
-<x-base-layout bodyClass="page-signup" title="Signup">
-    <main>
-        <div class="container-small page-login">
-            <div class="flex" style="gap: 5rem">
-                <div class="auth-page-form">
-                    <div class="text-center">
-                        <a href="/">
-                            <img src="/img/logoipsum-265.svg" alt="" />
-                        </a>
-                    </div>
-                    <h1 class="auth-page-title">Signup</h1>
+<x-guest-layout bodyClass="page-signup" title="Signup">
+
+    <h1 class="auth-page-title">Signup</h1>
 
                     <form action="" method="post">
                         <div class="form-group">
@@ -31,27 +23,11 @@
                             <input type="text" placeholder="Phone" />
                         </div>
                         <button class="btn btn-primary btn-login w-full">Register</button>
-
-                        <div class="grid grid-cols-2 gap-1 social-auth-buttons">
-                            <button class="btn btn-default flex justify-center items-center gap-1">
-                                <img src="/img/google.png" alt="" style="width: 20px" />
-                                Google
-                            </button>
-                            <button class="btn btn-default flex justify-center items-center gap-1">
-                                <img src="/img/facebook.png" alt="" style="width: 20px" />
-                                Facebook
-                            </button>
-                        </div>
-                        <div class="login-text-dont-have-account">
-                            Already have an account? -
-                            <a href="/login.html"> Click here to login </a>
-                        </div>
                     </form>
-                </div>
-                <div class="auth-page-image">
-                    <img src="/img/car-png-39071.png" alt="" class="img-responsive" />
-                </div>
-            </div>
-        </div>
-    </main>
-</x-base-layout>
+
+                    <x-slot:footerLink>
+                        Already have an account? -
+                            <a href="/login.html"> Click here to login </a>
+                    </x-slot:footerLink>
+
+</x-guest-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,13 +3,6 @@
 <x-base-layout :$title>
     <x-layouts.header />
 
-
     {{ $slot }}
-
-    <footer>
-        <a href="http://">Link 1</a>
-        <a href="http://">Link 2</a>
-        {{ $footerLinks }}
-    </footer>
 
 </x-base-layout>
